bf94a857da497addf139846ec7ea27a74cdde1b2
최용권 <puzzle75@naver.com>
2023-09-14 09:48:42 +0900

    logging 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -192,6 +192,10 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
-    }
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
+    },
 }
 

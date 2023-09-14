053a31aadb8158b0012a354a5cdff274b6bb3d09
최용권 <puzzle75@naver.com>
2023-09-14 10:50:42 +0900

    도메인 설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.200.167.86']
+ALLOWED_HOSTS = ['43.200.167.86', 'pybo-cyk.pe.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
